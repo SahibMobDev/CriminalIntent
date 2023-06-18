a4158a3ac78bbeda494891a0c54202f69fdf04b0
SahibMobDev <sahibmobdev@gmail.com>
2023-06-18 17:38:25 +0400

    RecyclerView adapter

--- app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -2,14 +2,23 @@ package com.example.criminalintent
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminalintent.databinding.ListItemCrimeBinding
 
-class CrimeHolder(val binding: ListItemCrimeBinding): RecyclerView.ViewHolder(binding.root) {
+class CrimeHolder(private val binding: ListItemCrimeBinding): RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
 
+        binding.root.setOnClickListener {
+            Toast.makeText(binding.root.context, "${crime.title} is clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
 
 class CrimeListAdapter(private val crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
@@ -22,10 +31,6 @@ class CrimeListAdapter(private val crimes: List<Crime>): RecyclerView.Adapter<Cr
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
-        }
+        holder.bind(crime)
     }
-
 }
