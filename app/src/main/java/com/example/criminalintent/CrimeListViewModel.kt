0cef915236394ed2b7cefd6c673217228c7171c3
SahibMobDev <sahibmobdev@gmail.com>
2023-06-20 00:16:44 +0400

    Coroutines and Databases

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,14 +1,33 @@
 package com.example.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
+
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init starting")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+            crimes += loadCrime()
+            Log.d(TAG, "loading crimes finished")
+        }
+
+    }
+
+    suspend fun loadCrime(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -16,7 +35,8 @@ class CrimeListViewModel : ViewModel() {
                 date = Date(),
                 isSolved = i % 2 == 0
             )
-            crimes += crime
+            result += crime
         }
+        return result
     }
 }
